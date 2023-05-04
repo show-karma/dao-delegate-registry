b82a9c033eca1da9dc03ddd538d7505632d39f82
Mahesh Murthy <mahesh@mmurthy.com>
2023-05-04 12:39:43 -0700

    Send token id and network id in delegate delegate event Update variable names

--- contracts/DelegateRegistry.sol
+++ contracts/DelegateRegistry.sol
@@ -10,11 +10,11 @@ contract DelegateRegistry {
 
     mapping(address => mapping(address => mapping(uint256 => Delegate))) public delegates;
 
-    event DelegateAdded(address indexed delegateAddress, address tokenAddress, uint256 tokenChainId, bytes metadata);
-    event DelegateRemoved(address indexed delegateAddress);
-    event DelegateMetadataUpdated(address indexed delegateAddress, address tokenAddress, uint256 chainId, bytes metadata);
+    event DelegateAdded(address indexed delegateAddress, address tokenAddress, uint256 tokenChainId, string metadata);
+    event DelegateRemoved(address indexed delegateAddress, address tokenAddress, uint256 tokenChainId);
+    event DelegateMetadataUpdated(address indexed delegateAddress, address tokenAddress, uint256 tokenChainId, string metadata);
 
-    function registerDelegate(address _tokenAddress, uint256 _tokenChainId, bytes memory _metadata) public {
+    function registerDelegate(address _tokenAddress, uint256 _tokenChainId, string memory _metadata) public {
         Delegate memory newDelegate = Delegate({
             delegateAddress: msg.sender,
             tokenAddress: _tokenAddress,
@@ -33,14 +33,14 @@ contract DelegateRegistry {
         Delegate memory removedDelegate = delegates[delegateAddress][delegateAddress][0];
         delete delegates[delegateAddress][removedDelegate.tokenAddress][removedDelegate.tokenChainId];
 
-        emit DelegateRemoved(removedDelegate.delegateAddress);
+        emit DelegateRemoved(removedDelegate.delegateAddress, removedDelegate.tokenAddress, removedDelegate.tokenChainId);
     }
     
-    function updateDelegateMetadata(address _tokenAddress, uint256 _chainId, bytes memory _metadata) public {
-        Delegate storage delegateToUpdate = delegates[msg.sender][_tokenAddress][_chainId];
+    function updateDelegateMetadata(address _tokenAddress, uint256 _tokenChainId, string memory _metadata) public {
+        Delegate storage delegateToUpdate = delegates[msg.sender][_tokenAddress][_tokenChainId];
         require(delegateToUpdate.delegateAddress == msg.sender, "Delegate does not exist for this address, token address, and chain ID");
 
-        emit DelegateMetadataUpdated(msg.sender, _tokenAddress, _chainId, _metadata);
+        emit DelegateMetadataUpdated(msg.sender, _tokenAddress, _tokenChainId, _metadata);
     }
 
     function getDelegate(address _delegateAddress, address _tokenAddress, uint256 _tokenChainId) public view returns (Delegate memory) {
