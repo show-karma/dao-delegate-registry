7aea8cf18ed51cb38358a9a0b103604817435e42
Mahesh Murthy <mahesh@mmurthy.com>
2023-11-27 20:51:38 -0800

    Score Registry deployment

--- smart-contracts/contracts/ScoreRegistry.sol
+++ smart-contracts/contracts/ScoreRegistry.sol
@@ -2,32 +2,19 @@
 
 pragma solidity ^0.8.19;
 
-import {Stats} from "./Common.sol";
+import {Stats} from "./ScoreStats.sol";
 import {IDelegateRegistry} from "./IDelegateRegistry.sol";
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ScoreRegistry is Initializable, OwnableUpgradeable, EIP712Upgradeable {
-    mapping(address => uint256) private whitelist;
+contract ScoreRegistry is Ownable {
     mapping(address => mapping(address => mapping(uint256 => Stats)))
         private stats;
 
     IDelegateRegistry private delegateRegistry;
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize(IDelegateRegistry _delegateRegistry)
-        public
-        initializer
-    {
+    constructor(IDelegateRegistry _delegateRegistry) {
         delegateRegistry = _delegateRegistry;
-        __Ownable_init();
     }
 
     function setDelegateRegistry(IDelegateRegistry _delegateRegistry)
@@ -39,18 +26,15 @@ contract ScoreRegistry is Initializable, OwnableUpgradeable, EIP712Upgradeable {
 
     function setStats(
         Stats calldata payload,
-        address user,
         address token,
         uint32 chainId
     ) public {
-        
-        require(whitelist[msg.sender] == 1, "sender is not whitelisted");
         require(
-            isDelegateRegistered(user, token, chainId) == 1,
+            isDelegateRegistered(msg.sender, token, chainId) == 1,
             "delegate is not registered"
         );
 
-        stats[user][token][chainId] = payload;
+        stats[msg.sender][token][chainId] = payload;
     }
 
     function isDelegateRegistered(
@@ -64,14 +48,6 @@ contract ScoreRegistry is Initializable, OwnableUpgradeable, EIP712Upgradeable {
             chainId
         );
         return isRegistered;
-    }
-
-    function setWhitelist(address user, uint256 status) public onlyOwner {
-        whitelist[user] = status;
-    }
-
-    function getWhitelist(address user) public view returns (uint256) {
-        return whitelist[user];
     }
 
     function getStats(
